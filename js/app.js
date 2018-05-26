cab7c0a731a418072ef03d62928c9384283e5b04
Oscar Carballal <oscar.carballal@gmail.com>
2018-05-27 00:09:02 +0200

    Finished v14

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+
+function toggleDetails(el){
+    $(el).find('.tags-section').slideToggle(300, 'linear');
+    $(el).find('.description-section').slideToggle(300, 'linear');
+}
+
+function expandCollapse(){
+    if ($('.expand-collapse').text() == 'expand all') {
+        $('.tags-section:hidden').slideDown();
+        $('.description-section:hidden').slideDown();
+        $('.expand-collapse').html("collapse all<i class='icon-angle-circled-up'>");
+    } else {
+        $('.tags-section').slideUp();
+        $('.description-section').slideUp();
+        $('.expand-collapse').html("expand all<i class='icon-angle-circled-down'>");
+    }
+}
